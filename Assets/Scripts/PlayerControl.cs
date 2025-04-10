4ad239406e2c42d25367be91efe3129d384ee4c5
insu57 <firejohnson57@gmail.com>
2025-04-10 17:13:23 +0900

    플레이어 조작 수정
    플레이어 조작 수정
    에셋 수정

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -12,7 +12,8 @@ public class PlayerControl : MonoBehaviour
    private Camera _mainCamera;
    private Rigidbody2D _rigidbody;
    private Vector2 _playerInput;
-
+   private bool _isFlipped = false;
+   
    public event Action<bool> OnPlayerMove;
    public event Action<bool> OnPlayerShot;
    public event Action<bool> OnPlayerReload;
@@ -27,7 +28,7 @@ public class PlayerControl : MonoBehaviour
       _mainCamera = Camera.main;
    }
 
-   private void LateUpdate()
+   private void LateUpdate() //애니메이션 적용이후라 팔 움직임... -> AvatarMask로?
    {
       Vector3 mousePos = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
       mousePos.z = 0;
@@ -36,8 +37,21 @@ public class PlayerControl : MonoBehaviour
       Debug.DrawRay(rightArm.transform.position, direction, Color.red);
       
       float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-      
+
+      if (_isFlipped)
+      {
+         if (angle > 0)
+         {
+            angle = 180 - angle;
+         }
+         else
+         {
+            angle = -180 - angle;
+         }
+      }
+      angle = Mathf.Clamp(angle, -60f, 60f);
       rightArm.transform.localRotation = Quaternion.Euler(0, 0, -angle);
+      //avatar mask, flip적용(좌측도)
    }
 
    private void FixedUpdate()
@@ -58,15 +72,14 @@ public class PlayerControl : MonoBehaviour
 
       if (_playerInput.x == 0)
       {
-         //_playerAnimationManager.IsWalk = false;
          OnPlayerMove?.Invoke(false);
          return;
       }
+      
       if(_playerInput.y != 0) return;
-      playerScale.x = Mathf.Abs(playerScale.x) * _playerInput.x;// 기본 좌측(InputX == -1) 우측 반전(Input == 1)
-      Debug.Log(_playerInput);
+      playerScale.x = Mathf.Abs(playerScale.x) * _playerInput.x;
+      _isFlipped = _playerInput.x < 0; //왼쪽입력 시 Flip
       playerSprite.transform.localScale = playerScale;
-      //_playerAnimationManager.IsWalk = true;
       OnPlayerMove?.Invoke(true);
    }
     
