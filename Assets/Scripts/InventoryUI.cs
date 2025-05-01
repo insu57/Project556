80bf63eb1dd87f5be17d96467c6aa08efa4ec93b
insu57 <firejohnson57@gmail.com>
2025-05-01 16:24:51 +0900

    인벤토리 구현중, 에셋 수정
    인벤토리 구현 중.(아이템 이동 기본 기능 구현 완료, AxB크기 아이템 구현 중)
    스프라이트 애셋 수정

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -1,23 +1,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
 
 public class InventoryUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     //8x8 test... 
+    
     [SerializeField] private GameObject inventoryGrid;
-    [SerializeField] private GameObject inventoryCellPrefab;
+    [SerializeField] private GameObject inventorySlotPrefab;
     [SerializeField] private int inventoryXSize;
     [SerializeField] private int inventoryYSize;
+    public RectTransform InventoryGridRT { get; private set; }
+
+    private List<SlotData> _slotDataList = new List<SlotData>();
+    //List or Array 고민... (인벤토리 생성 초기 이후 크기가 변하지 않을 수 있음...)
     
     [SerializeField] private GameObject item;
     private Canvas _rootCanvas;
     private RectTransform _itemRectTransform;
     private Vector2 _pointerOffset;
+    private static readonly Vector2 StartMinPos = new Vector2(0, 0);
+    private static readonly Vector2 StartMaxPos = new Vector2(100, -100);
+    private static readonly Vector2 StartImagePos = new Vector2(50, -50);
 
+    private struct SlotData
+    {
+        public bool IsEmpty;
+        public Vector2 MinPosition;
+        public Vector2 MaxPosition;
+        public Vector2 ImagePosition;
+
+        public SlotData(bool isEmpty, Vector2 minPosition, Vector2 maxPosition, Vector2 imagePosition)
+        {
+            IsEmpty = isEmpty;
+            MinPosition = minPosition;
+            MaxPosition = maxPosition;
+            ImagePosition = imagePosition;
+        }
+        //
+    }
+        
     private void Awake()
     {
         _rootCanvas = GetComponentInParent<Canvas>();
+        InventoryGridRT = inventoryGrid.GetComponent<RectTransform>();
     }
     
     private void Start()
@@ -28,27 +56,87 @@ public class InventoryUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         
         gridRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, inventoryXSize*100);
         gridRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, inventoryYSize*100);
-
-        for (int i = 0; i < inventoryXSize*inventoryYSize; i++)
+    
+        //Index 0 (pivot 0.5, 0.5) -> 50, -50
+        // (0, 0) ~ (100, -100) Slot RectTransform 범위...
+        
+        Vector2 cellOffset = new Vector2(100, -100);
+        
+        //
+        for (int y = 0; y < inventoryYSize; y++)
         {
-            Instantiate(inventoryCellPrefab, inventoryGrid.transform);
+            for (int x = 0; x < inventoryXSize; x++)
+            {
+                Instantiate(inventorySlotPrefab, inventoryGrid.transform);
+                
+                Vector2 offset = new Vector2(x, y) * cellOffset;
+                Vector2 minPos = StartMinPos + offset;
+                Vector2 maxPos = StartMaxPos + offset;
+                Vector2 imagePos = StartImagePos + offset;
+                
+                _slotDataList.Add(
+                    new SlotData( true, minPos, maxPos, imagePos));
+            } 
         }
-        //인벤토리 크기 만큼 Cell생성
-        
-        
+        _itemRectTransform = item.GetComponent<RectTransform>();
+        //0~N*M
+        const int index = 23;
+        int a = index % inventoryXSize;
+        int b = index / inventoryXSize;
+        var temp = _slotDataList[index];
+        temp.IsEmpty = false;
+        _slotDataList[index] = temp;
+        _itemRectTransform.anchoredPosition = _slotDataList[index].ImagePosition;
+        Debug.Log(_slotDataList[index].IsEmpty);
         //NxM 인벤토리(N*M만큼 Cell생성), AxB크기 아이템, 드래그 이동(마우스 입력처리), 칸 점유 정보
         //i, j -> 100, -100
-        _itemRectTransform = item.GetComponent<RectTransform>();
-        //15번째로...(0~63)
-        const int index = 23;
-        int x = index % inventoryXSize;
-        int y = index / inventoryYSize;
-        var vector2 = _itemRectTransform.anchoredPosition;
-        vector2.x = x * 100;
-        vector2.y = y * -100;
-        //_itemRectTransform.anchoredPosition = vector2;
+
+        //int x = index % inventoryXSize;
+        //int y = index / inventoryYSize;
+
+        //Vector2 zeroPos = new Vector2(50, -50);
+        //_itemRectTransform.anchoredPosition = zeroPos + new Vector2(x * 100, y * -100);
+
     }
 
+    private int GetSlotIndex(Vector2 pos)
+    {
+        int x = (int)(pos.x / 100);
+        int y = -(int)(pos.y / 100);
+        return x + y * inventoryYSize;
+    }
+
+    public bool SlotEmptyCheck(Vector2 pos)
+    {
+        int idx = GetSlotIndex(pos);
+        
+        if (idx < 0 || idx >= inventoryXSize * inventoryYSize)
+        {
+            return false;
+        }
+        
+        bool isEmpty = _slotDataList[idx].IsEmpty;
+        
+        return isEmpty;
+    }
+
+    public Vector2 ItemMove(Vector2 startPos, Vector2 endPos)
+    {
+        int startIdx = GetSlotIndex(startPos);
+        int endIdx = GetSlotIndex(endPos);
+        
+        var temp = _slotDataList[startIdx];
+        temp.IsEmpty = true;
+        _slotDataList[startIdx] = temp;
+        
+        temp = _slotDataList[endIdx];
+        temp.IsEmpty = false;
+        _slotDataList[endIdx] = temp;
+        
+        return _slotDataList[endIdx].ImagePosition;
+    }
+    
+    
     public void OnPointerEnter(PointerEventData eventData)
     {
         
--- Assets/Scripts/ItemDragger.cs
+++ Assets/Scripts/ItemDragger.cs
@@ -3,45 +3,57 @@ using UnityEngine.EventSystems;
 
 public class ItemDragger : MonoBehaviour, IPointerDownHandler, IDragHandler, IEndDragHandler
 {
-    private RectTransform _rectTransform;
+    private RectTransform _itemRT;
+    private RectTransform _inventoryRT;
     private Canvas _rootCanvas;
-    private RectTransform _rootRect;
     private Vector2 _pointerOffset;
     private Vector2 _pointerDownPos;
     private CanvasGroup _canvasGroup;
     
-    private Vector2 _halfSize;
-    private Vector2 _screenHalfSize;
+    private InventoryUI _inventoryUI;
     
     //anchor min 0.5, 0.5 max 0.5, 0.5 pivot 0.5, 0.5  
     private void Awake()
     {
-        _rectTransform = GetComponent<RectTransform>();
+        _itemRT = GetComponent<RectTransform>();
         _rootCanvas = GetComponentInParent<Canvas>();
-        _rootRect = _rootCanvas.transform as RectTransform;
         _canvasGroup = GetComponent<CanvasGroup>();
         
-        _halfSize = _rectTransform.sizeDelta * 0.5f;
-        _screenHalfSize = new Vector2(960f, 540f); //1920x1080 Half
+        _inventoryUI = GetComponentInParent<InventoryUI>();
+        _inventoryRT = _inventoryUI.GetComponent<RectTransform>();
     }
     
     public void OnPointerDown(PointerEventData eventData)
     {
         _canvasGroup.blocksRaycasts = false;
+        _pointerDownPos = _itemRT.anchoredPosition;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         if (RectTransformUtility.ScreenPointToWorldPointInRectangle(
-                _rectTransform, eventData.position, eventData.pressEventCamera, out var globalMousePos))
+                _itemRT, eventData.position, eventData.pressEventCamera, out var globalMousePos))
         {
-            _rectTransform.position = globalMousePos;
+            _itemRT.position = globalMousePos;
         }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        _rectTransform.anchoredPosition = _pointerDownPos;
+        //check position
+        if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                _inventoryUI.InventoryGridRT, eventData.position, eventData.pressEventCamera, out var localPoint
+            )) return;
+
+        if (!_inventoryUI.SlotEmptyCheck(localPoint)) //Empty가 아니거나 인벤토리 밖
+        {
+            _itemRT.anchoredPosition = _pointerDownPos;
+        }
+        else
+        {
+           _itemRT.anchoredPosition = _inventoryUI.ItemMove(_pointerDownPos, localPoint);
+        }
+
         _canvasGroup.blocksRaycasts = true;
     }
     
